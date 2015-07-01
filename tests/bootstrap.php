2f773e3fa02741ce657a9806ad7aff57c7c32358
ADmad <ADmad@users.noreply.github.com>
2015-07-01 20:56:41 +0530

    Update tests bootstrap.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -16,18 +16,11 @@ $findRoot = function ($root) {
             return $root;
         }
     } while ($root !== $lastRoot);
-
     throw new Exception("Cannot find the root of the application, unable to run tests");
 };
 $root = $findRoot(__FILE__);
 unset($findRoot);
-
 chdir($root);
-if (file_exists($root . '/config/bootstrap.php')) {
-    require $root . '/config/bootstrap.php';
-    return;
-}
-
 require $root . '/vendor/cakephp/cakephp/tests/bootstrap.php';
 
 Plugin::load('ADmad/JwtAuth', ['path' => dirname(dirname(__FILE__)) . DS]);
